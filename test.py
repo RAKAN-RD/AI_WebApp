8e12718e57a9842206cac929162b8799e30af894
RAKAN-RD <rakan.23108@mhs.unesa.ac.id>
2024-11-15 11:46:51 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+Judul = st.title("Hello World!", anchor='right')
+Sub = st.subheader("The World is so beautifull.")
+st.header("World is so complex.")
+st.text("Earth is Our World. So if the earth not match with our condition anymore, we would die.")
+st.markdown("[YouTube](https://www.youtube.com)")
