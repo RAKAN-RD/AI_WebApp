91ae768e234dec0528568c990a15a492230dc758
RAKAN-RD <rakan.23108@mhs.unesa.ac.id>
2024-11-15 11:52:30 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-Judul = st.title("Hello World!", anchor='right')
+Judul = st.title("Hello World!", anchor='left')
 Sub = st.subheader("The World is so beautifull.")
 st.header("World is so complex.")
 st.text("Earth is Our World. So if the earth not match with our condition anymore, we would die.")
