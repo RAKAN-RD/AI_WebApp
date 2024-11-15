701860801c918cdd13165eaf709da46c663cff46
RAKAN-RD <rakan.23108@mhs.unesa.ac.id>
2024-11-15 11:59:42 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ Sub = st.subheader("The World is so beautifull.")
 st.header("World is so complex.")
 st.text("Earth is Our World. So if the earth not match with our condition anymore, we would die.")
 st.markdown("[YouTube](https://www.youtube.com)")
+
+Main_Menu()
