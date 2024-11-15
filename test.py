4152e5b72d6ce17750a331ac1f76be75d68e6653
RAKAN-RD <rakan.23108@mhs.unesa.ac.id>
2024-11-15 11:58:13 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import streamlit as st
-import uji_web
-#Judul = st.title("Hello World!", anchor='left')
-#Sub = st.subheader("The World is so beautifull.")
-#st.header("World is so complex.")
-#st.text("Earth is Our World. So if the earth not match with our condition anymore, we would die.")
-#st.markdown("[YouTube](https://www.youtube.com)")
+from uji_web import MainApp
+
+
+Judul = st.title("Hello World!", anchor='left')
+Sub = st.subheader("The World is so beautifull.")
+st.header("World is so complex.")
+st.text("Earth is Our World. So if the earth not match with our condition anymore, we would die.")
+st.markdown("[YouTube](https://www.youtube.com)")
